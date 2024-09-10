9d971b8e5dff9cae86f8b687bda00bde584de41f
NHHieuu <hieuhong08122003@gmail.com>
2024-09-10 23:21:37 +0700

    css

---
+++ HTML/html.cs
@@ -0,0 +1,23 @@
+st
+body {
+  background-color: lightblue;
+}
+
+p {
+  font-family: verdana;
+  font-size: 20px;
+}
+h1 {
+  text-align: center;
+  color: red;
+}
+
+h2 {
+  text-align: center;
+  color: red;
+}
+
+p {
+  text-align: center;
+  color: red;
+}
